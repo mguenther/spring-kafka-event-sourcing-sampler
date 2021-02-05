847a5a34d5e9693d632222d26016ca1f9b0bef4f
Markus Günther <markus.guenther@gmail.com>
2021-02-05 21:40:38 +0100

    Fixes Spring Cloud Gateway configuration

--- gtd-api-gateway/src/main/java/net/mguenther/gtd/GtdApiGatewayApplication.java
+++ gtd-api-gateway/src/main/java/net/mguenther/gtd/GtdApiGatewayApplication.java
@@ -1,27 +1,20 @@
 package net.mguenther.gtd;
 
-import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
 
 /**
  * @author Markus Günther (markus.guenther@gmail.com)
  * @author Boris Fresow (bfresow@gmail.com)
  */
 @SpringBootApplication
-@EnableZuulProxy
+@EnableDiscoveryClient
 @EnableEurekaClient
 public class GtdApiGatewayApplication {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(GtdApiGatewayApplication.class).web(WebApplicationType.SERVLET).run(args);
-    }
-
-    @Bean
-    public OnMethodFilter onMethodZuulFilter() {
-        return new OnMethodFilter();
+        SpringApplication.run(GtdApiGatewayApplication.class, args);
     }
 }
---
+++ gtd-api-gateway/src/main/java/net/mguenther/gtd/SpringCloudConfig.java
@@ -0,0 +1,23 @@
+package net.mguenther.gtd;
+
+import com.netflix.discovery.DiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+
+@Configuration
+public class SpringCloudConfig {
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.method(HttpMethod.DELETE, HttpMethod.PUT, HttpMethod.POST, HttpMethod.PATCH)
+                        .uri("lb://gtd-es-command-side"))
+                .route(r -> r.method(HttpMethod.GET)
+                        .uri("lb://gtd-es-query-side"))
+                .build();
+    }
+}
